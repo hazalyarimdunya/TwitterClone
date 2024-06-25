2ce779131056045ac2d2002243c918535124a438
Hazal Yarımdünya <hazalyarimdunya@hotmail.com>
2024-06-25 16:25:00 +0300

    project committed

---
+++ TwitterClone/MainTabbarViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TwitterClone
+//
+//  Created by PC on 25.06.2024.
+//
+
+import UIKit
+
+class MainTabbarViewController: UITabBarController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .systemBlue
+    }
+
+
+}
+
